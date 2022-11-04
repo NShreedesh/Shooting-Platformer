80b7b02016771dede7abf6797d51a70c9f17fcae
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-11-04 08:31:30 +0545

    add rotate towards crosshair

---
+++ Assets/Scripts/Gun/RotateTowardsPlayer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RotateTowardsPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void Update()
+    {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
