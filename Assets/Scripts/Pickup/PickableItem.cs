8bc9d69bdb59a44ffdafd502186a4fea606e0962
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-11-24 16:59:07 +0545

    serprated gun and pickableItem script

--- Assets/Scripts/Pickup/PickableItem.cs
+++ Assets/Scripts/Pickup/PickableItem.cs
@@ -5,27 +5,10 @@ public class PickableItem : MonoBehaviour, IPickable
     [field: SerializeField]
     public bool IsPicked { get; private set; }
 
-    [SerializeField]
-    private BoxCollider2D boxCollider;
-
-    private void Start()
-    {
-        ToggleCollider();
-    }
-
-    public void Pick()
+    public virtual void Pick()
     {
         if (IsPicked) return;
 
         IsPicked = true; 
-        ToggleCollider();
-    }
-
-    private void ToggleCollider()
-    {
-        if (IsPicked)
-            boxCollider.enabled = false;
-        else
-            boxCollider.enabled = true;
     }
 }
--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -2,10 +2,33 @@
 
 public class Gun : PickableItem
 {
+    [Header("Components")]
+    private BoxCollider2D boxCollider;
+
     [Header("Gun Data Object")]
     public GunScriptableObject gunData;
 
     [Header("Data to be Given")]
     public Transform shootPoint;
     public GameObject muzzleFlash;
+
+    private void Start()
+    {
+        boxCollider= GetComponent<BoxCollider2D>();
+        ToggleCollider();
+    }
+
+    public override void Pick()
+    {
+        base.Pick();
+        ToggleCollider();
+    }
+
+    private void ToggleCollider()
+    {
+        if (IsPicked)
+            boxCollider.enabled = false;
+        else
+            boxCollider.enabled = true;
+    }
 }
